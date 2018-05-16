fceb980ca798171d2fe96e2b7204e11fcb9f7e99
zhyjor <zhyjor@163.com>
2018-05-16 15:46:49 +0800

    add:drawer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+
 import router from './router'
 import './assets/reset.css'
 import './assets/prod.css'
+import App from './App'
+import DrawerLayout from 'vue-drawer-layout'
+
+Vue.use(DrawerLayout)
 
 Vue.config.productionTip = false
 
@@ -13,5 +17,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import root from '@/components/root'
+import draw from '@/components/drawerLayout'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'root',
       component: root
+    },
+    {
+      path: '/draw',
+      name: 'draw',
+      component: draw
     }
   ]
 })
